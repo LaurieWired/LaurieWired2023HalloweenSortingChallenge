3081cf0d8bcae4efd465f9c4c517639524c01362
HoLAI <37847231+R00dNet@users.noreply.github.com>
2023-10-24 23:54:40 +0100

    Update sort.cpp

--- code_stubs/sort.cpp
+++ code_stubs/sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <unistd.h>
 
 void print_array(const std::vector<int>& arr) {
     std::cout << "{" << std::endl;
@@ -13,10 +14,24 @@ void print_array(const std::vector<int>& arr) {
     std::cout << std::endl << "}" << std::endl;
 }
 
-void sort(std::vector<int>& arr) {
-    // Your code goes here!
+void quicksort(std::vector<int>& arr, int low, int high) {
+    if (low >= high) return;
+    int pivot = arr[high];
+    int i = low;
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
+            std::swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+    std::swap(arr[i], arr[high]);
+    print_array(arr);
+    quicksort(arr, low, i - 1);
+    quicksort(arr, i + 1, high);
+}
 
-    print_array(arr);  // Make sure to call this print after every iteration
+void sort(std::vector<int>& arr) {
+    quicksort(arr, 0, arr.size() - 1);
 }
 
 int main() {
